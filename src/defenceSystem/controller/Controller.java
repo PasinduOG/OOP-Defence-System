4b6c4e0c5219d5691143c497c5ffd5edf688b5d4
Pasindu Madhuwantha <126347762+PasinduOG@users.noreply.github.com>
2025-11-18 19:09:06 +0530

    Added ArrayList with private access for only controller and added public methods to add, update and use

--- src/defenceSystem/controller/Controller.java
+++ src/defenceSystem/controller/Controller.java
@@ -4,5 +4,27 @@ import defenceSystem.model.DefenceObserver;
 import java.util.ArrayList;
 
 public class Controller<T extends DefenceObserver> {
-    ArrayList<T> defenceList = new ArrayList<>();
+    private ArrayList<T> defenceList = new ArrayList<>();
+    
+    public void addDefence(T obj){
+        defenceList.add(obj);
+    }
+    
+    public void updateDefence(String message){
+        for (T obj : defenceList) {
+            obj.updateMessage(message);
+        }
+    }
+    
+    public void setAreaClearMessage(boolean clear){
+        for (T obj : defenceList) {
+            obj.clearArea(clear);
+        }
+    }
+    
+    public void enableButtons(int value){
+        for (T obj : defenceList) {
+            obj.enableButtons(value);
+        }
+    }
 }
